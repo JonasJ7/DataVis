dbd774e81915d13240e563d859a0ffe3994688a3
JonasJ7 <jonasjuhlert7@gmail.com>
2022-03-10 13:25:21 +0100

    fixed .js implementation

--- index.js
+++ index.js
@@ -1,38 +1,23 @@
-var data = [
-	{ name: 'data1', value: 10 },
-	{ name: 'data2', value: 20 },
-	{ name: 'data2', value: 30 }
-];
+var w = 1000;
+var h = 1000;
+var rad = 50;
+var leftMargin = w / 4;
+var dogdata = [10, 5, 30, 20].sort(d3.ascending);
 
-var width = 800;
-var height = 400;
-var margin = { top: 50, bottom: 50, left: 50, right: 50 }
+var svg = d3.select("svg").attr("width", w).attr("height", h).style("background-color", "black");
 
-var svg = d3.select('#canvas')
-	.append('svg')
-	.attr('height', height - margin.top - margin.bottom)
-	.attr('width', width - margin.left - margin.right)
-	.attr('viewBox', [0, 0, width, height]);
-
-var x = d3.scaleBand()
-	.domain(d3.range(data.length))
-	.range([margin.left, width - margin.right])
-	.padding(.1);
-
-var y = d3.scaleLinear()
-	.domain([0, 100])
-	.range([height - margin.bottom, margin.top]);
-
-svg
-	.append('g')
-	.attr('fill', 'royalblue')
-	.selectAll('rect')
-	.data(data.sort((a, b) => d3.descending(a.value, b.value)
-		.join('rect')
-		.attr('x', (d, i) => x(i))
-		.attr('y', (d) => y(d.value))
-		.attr('height', d => y(0) - y(d.value))
-		.attr('width', x.bandwidth())
-
-svg.node();
-
+var circle = d3.select("svg").selectAll("circle")
+    .data(dogdata)
+    .join("circle")
+    .attr("cx", function (d, i) {
+        console.log(d);
+        console.log(i);
+        return leftMargin + i * rad * 2;
+    })
+    .attr("cy", h / 2)
+    .attr("r", function (d) {
+        return d * rad / 4;
+    })
+    .attr("fill", "none")
+    .attr("stroke", "white")
+    .style("stroke-width", "5");
